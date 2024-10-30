393545fba9de97b4d0119a9b189472d29a337ea9
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-10-30 09:33:25 -0300

    Aula 30 de Outubro

--- aula019_30out/backend.js
+++ aula019_30out/backend.js
@@ -4,11 +4,10 @@
 // `mongodb+srv://alexandreraminelli9491:404IleAQURtU1QHp@cluster0.bmq1r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const express = require("express")
-// Cross Origin:
-const cors = require("cors")
-// Mongoose:
+const cors = require("cors") // Cross Origin
 const mongoose = require("mongoose")
-
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const uniqueValidator = require("mongoose-unique-validator")
 
 const app = express()
@@ -38,22 +37,9 @@ const Usuario = mongoose.model("usuario", usuarioSchema)
 
 // Conexão com o banco de dados
 async function conectarAoMongo() {
-  // await mongoose.connect(`mongodb+srv://alexandreraminelli9491:404IleAQURtU1QHp@cluster0.bmq1r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-  await mongoose.connect(`mongodb+srv://pro_mac:mongo123@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+  await mongoose.connect(`mongodb+srv://alexandreraminelli9491:404IleAQURtU1QHp@cluster0.bmq1r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+  // await mongoose.connect(`mongodb+srv://pro_mac:mongo123@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 }
-
-let filmes = [
-  // Objeto filme 1
-  {
-    titulo: "Deadpool & Wolverine",
-    sinopse: "Wolverine está se recuperando quando cruza seu caminho com Deadpool. Juntos, eles formam uma equipe e enfrentam um inimigo em comum.",
-  },
-  // Objeto filme 2
-  {
-    titulo: "Oppenheimer",
-    sinopse: "O físico J. Robert Oppenheimer trabalha com uma equipe de cientistas durante o Projeto Manhattan, levando ao desenvolvimento da bomba atômica.",
-  },
-]
 
 // Buscar filmes
 app.get("/filmes", async (req, res) => {
@@ -81,18 +67,56 @@ app.post("/filmes", async (req, res) => {
 })
 
 app.post("/signup", async (req, res) => {
-  // Obter a entrada do usuário
+  try {
+    // Obter a entrada do usuário
+    const login = req.body.login
+    const password = req.body.password
+    // Hashear a senha
+    const senhaCriptografada = await bcrypt.hash(password, 10)
+
+    const usuario = new Usuario({
+      login: login,
+      password: senhaCriptografada,
+    })
+    const respMongo = await usuario.save()
+    console.log(respMongo)
+
+    // Finalizar requisição e evitar timeout
+    res.status(201).end()
+  } catch (e) {
+    console.log(e) // Exibir mensagem de erro
+    res.status(409).end()
+  }
+})
+
+// Verificar o login
+app.post("/login", async (req, res) => {
+  // Obter dados do formulário
   const login = req.body.login
   const password = req.body.password
 
-  const usuario = new Usuario({
-    login: login,
-    password: password,
-  })
-  const respMongo = await usuario.save()
-  console.log(respMongo)
-  // Finalizar requisição e evitar timeout
-  res.end()
+  // Verificar se o usuário existe
+  const usuarioExiste = await Usuario.findOne({ login: login })
+  // Se usuário não existir
+  if (!usuarioExiste) {
+    return res.status(404).json({ mensagem: "login inválido" })
+  }
+
+  // Verificar se a senha está correta
+  const senhaValida = await bcrypt.compare(password, usuarioExiste.password)
+  // Se senha for inválida
+  if (!senhaValida) {
+    return res.status(401).json({ mensagem: "senha inválida" })
+  }
+
+  // Gerar o token
+  const token = jwt.sign(
+    { login: login }, // objeto JSON com login do usuário
+    "chave-secreta",
+    { expiresIn: "1h" } // tempo de expiração: 1 hora
+  )
+  // Retornar status
+  res.status(200).json({ token: token })
 })
 
 app.listen(3000, () => {
